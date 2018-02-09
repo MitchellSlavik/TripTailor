da2ae40e673dada716c3deeb8a77b00b73c3d93c
ColinKrist <colinkrist97@gmail.com>
2018-02-08 21:08:14 -0600

    add css and templates

---
+++ triptailor/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('',views.home),
+    path('profile/',views.profile),
+    path('trips/',views.trips),
+    path('dashboard/',views.dashboard),
+    path('login/',views.login)
+]
--- triptailor/views.py
+++ triptailor/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    data = {}
+    return render(request,"triptailor/home.html",data)
+
+def profile(request):
+    data = {
+        'hello' : "hello colin"
+    }
+    return render(request,"triptailor/profile.html",data)
+
+def trips(request):
+    data = {
+        'hello' : "hello jonathan"
+    }
+    return render(request,"triptailor/trips.html",data)
+
+def dashboard(request):
+    data = {
+        'hello' : "hello carly"
+    }
+    return render(request,"triptailor/dashboard.html",data)
+
+def login(request):
+    data = {
+        'hello' : "hello friend"
+    }
+    return render(request,"triptailor/login.html",data)
