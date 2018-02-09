16350a060b2e89f6d7d69c212efb3ed2d4747c6c
ColinKrist <colinkrist97@gmail.com>
2018-02-08 21:08:14 -0600

    add css and templates

--- triptailor/urls.py
+++ triptailor/urls.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.urls import path
 
 from . import views
@@ -5,3 +6,17 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+=======
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('',views.home),
+    path('profile/',views.profile),
+    path('trips/',views.trips),
+    path('dashboard/',views.dashboard),
+    path('login/',views.login)
+]
+>>>>>>> add css and templates
--- triptailor/views.py
+++ triptailor/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render
+<<<<<<< HEAD
 
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
+=======
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    data = {}
+    return render(request,"triptailor/home.html",data)
+
+def profile(request):
+    data = {
+        'hello' : "hello colin"
+    }
+    return render(request,"triptailor/profile.html",data)
+
+def trips(request):
+    data = {
+        'hello' : "hello jonathan"
+    }
+    return render(request,"triptailor/trips.html",data)
+
+def dashboard(request):
+    data = {
+        'hello' : "hello carly"
+    }
+    return render(request,"triptailor/dashboard.html",data)
+
+def login(request):
+    data = {
+        'hello' : "hello friend"
+    }
+    return render(request,"triptailor/login.html",data)
+>>>>>>> add css and templates
