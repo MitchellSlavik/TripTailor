4481402e4df84d6efd4197574c23b866dd666bc3
Mitchell Slavik <Mitchell.A.Slavik@gmail.com>
2018-02-12 02:07:25 +0000

    Model from slack completed.

--- triptailor/models.py
+++ triptailor/models.py
@@ -22,6 +22,10 @@ class Traveler(models.Model):
 	
 class Trip(models.Model):
 	name = model.CharFeild(max_length=100)
+	cost = model.DecimalField(max_digits=10, decimal_places=2)
+	maxNumTravelers = model.IntegerField()
+	date = model.DateField()
+	description = model.TextField()
 	guide = model.ForeignKey(
 		Guide,
 		on_delete=models.CASCADE,
@@ -30,6 +34,10 @@ class Trip(models.Model):
 	travelers = models.ManyToManyField(
 		Traveler,
 		through='Ticket'
+	)
+	categories = models.ManyToManyField(
+		Category,
+		related_name='trips'
 	)
 	def __str__(self):
 		return self.name
@@ -44,5 +52,39 @@ class Ticket(models.Model):
 		on_delete=models.CASCADE,
 	)
 	num_travelers = models.IntegerField()
+	def __str__(self):
+		return self.trip.name + ' ['+self.traveler.__str__()+'] ('+num_travelers+')'
 	
+class Category(models.Model):
+	name = model.CharFeild(max_length=100)
+	isCity = model.BooleanField(default=False)
+	def __str__(self):
+		return self.name
 	
+class Location(models.Model):
+	name = model.CharFeild(max_length=100)
+	lat = model.DecimalField(max_digits=14, decimal_places=10)
+	lng = model.DecimalField(max_digits=14, decimal_places=10)
+	description = model.TextField()
+	sequence = model.IntegerField()
+	trip = model.ForeignKey(
+		Trip,
+		related_name='locaitons'
+	)
+	def __str__(self):
+		return self.name
+	
+class Review:
+	title = model.CharFeild(max_length=100)
+	stars = model.IntegerField()
+	body = model.TextField()
+	traveler = model.ForeignKey(
+		Traveler,
+		related_name='reviews'
+	)
+	trip = model.ForeignKey(
+		Trip,
+		related_name='location'
+	)
+	def __str__(self):
+		return self.title
