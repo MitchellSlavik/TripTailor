9181b4de4415e2566ed94efb00ded3cdd05c0797
ColinKrist <colinkrist97@gmail.com>
2018-02-13 12:46:50 -0600

    fixing weird commit

--- triptailor/urls.py
+++ triptailor/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path
 
 from . import views
@@ -11,17 +10,3 @@ urlpatterns = [
     path('dashboard/',views.dashboard),
     path('login/',views.login)
 ]
-=======
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-    path('',views.home),
-    path('profile/',views.profile),
-    path('trips/',views.trips),
-    path('dashboard/',views.dashboard),
-    path('login/',views.login)
-]
->>>>>>> da2ae40e673dada716c3deeb8a77b00b73c3d93c
