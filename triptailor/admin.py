8911be5f0db640b8f5b71406c6e696561b82cfb1
ColinKrist <colinkrist97@gmail.com>
2018-02-14 17:20:53 -0600

    adding models to admin page for easy management

--- triptailor/admin.py
+++ triptailor/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+admin.site.register(models.Guide)
+admin.site.register(models.Traveler)
+admin.site.register(models.Ticket)
+admin.site.register(models.Trip)
+admin.site.register(models.Location)
+admin.site.register(models.Category)
