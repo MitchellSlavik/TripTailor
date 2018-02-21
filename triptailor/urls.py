e6fa39bc2593c9df5b06de93808f1e3870f74f79
ColinKrist <colinkrist97@gmail.com>
2018-02-21 17:49:17 -0600

    restruc - searchBar template added and added new template blocks

--- triptailor/urls.py
+++ triptailor/urls.py
@@ -6,11 +6,12 @@ from . import views
 
 urlpatterns = [
     path('',views.home),
-    path('searchtrip/',views.searchTrip),
+    path('searchtrip/',views.searchTrip, name='searchTrip'),
     path('profile/',views.profile),
     path('trips/',views.trips),
     path('dashboard/',views.dashboard),
     path('login/',views.login),
     path('createuser/',views.createUserPage),
-    path('createtrip/',views.createTrip),
+    path('createtrip/',views.createTrip, name='createtrip'),
+    path('postnewtrip/',views.postNewTrip,name="postTrip"),
 ]
